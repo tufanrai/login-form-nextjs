c4941d8b361854e04fd8dcd11e08f1a8c2d3ff19
TufanRai <thulungtufan16@gmail.com>
2025-05-22 06:47:07 +0545

    unwanted folder removed

--- schema/auth.ts
+++ schema/auth.ts
@@ -4,3 +4,15 @@ export const LoginSchema = yup.object({
   email: yup.string().email("enter valid email").required(),
   password: yup.string().required("Password is required"),
 });
+
+export const SignUpData = yup
+  .object({
+    full_name: yup.string().required("please enter your full name"),
+    email: yup.string().email().required("please enter a valid email"),
+    password: yup
+      .string()
+      .required("please enter your password")
+      .min(6, "password should be of at lease 6 characters"),
+    Confirm_password: yup.string().required("please re enter your password"),
+  })
+  .required();
